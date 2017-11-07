5227c7f199579ed3535d99859b769aa20f4f768e
Grant Fortington <gwfortington@hotmail.com>
2017-11-08 08:38:47 +1030

    Provide config_template.js and ignore config.js

---
+++ config_template.js
@@ -0,0 +1,11 @@
+module.exports = {
+  development: {
+    database: {
+      username: 'apps',
+      password: '********',
+      hostname: 'x.x.x.x',
+      port: '1521',
+      sid: 'EBSDB'
+    }
+  }
+}
