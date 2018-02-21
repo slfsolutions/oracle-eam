8cd9d5ae5220eaf452e7e302a1c7ab49544b5394
Grant Fortington <gwfortington@hotmail.com>
2018-02-21 15:47:28 +1030

    Include config

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  development: {
+    database: {
+      username: 'apps',
+      password: 'apps',
+      hostname: '129.154.74.184',
+      port: '1521',
+      serviceName: 'EBSDB'
+    }
+  }
+}
