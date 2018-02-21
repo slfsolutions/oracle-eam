a100f898755cc21907df438ec0214646a54a1912
Grant Fortington <gwfortington@hotmail.com>
2018-02-22 08:53:49 +1030

    Change config to use environment variables

--- config.js
+++ config.js
@@ -1,11 +1,20 @@
 module.exports = {
   development: {
     database: {
-      username: 'apps',
-      password: 'apps',
-      hostname: '129.154.74.184',
-      port: '1521',
-      serviceName: 'EBSDB'
+      username: process.env.NODE_ORACLE_EAM_DEVELOPMENT_DATABASE_USERNAME || '',
+      password: process.env.NODE_ORACLE_EAM_DEVELOPMENT_DATABASE_PASSWORD || '',
+      hostname: process.env.NODE_ORACLE_EAM_DEVELOPMENT_DATABASE_HOSTNAME || '',
+      port: process.env.NODE_ORACLE_EAM_DEVELOPMENT_DATABASE_PORT || '',
+      serviceName: process.env.NODE_ORACLE_EAM_DEVELOPMENT_DATABASE_SERVICE_NAME || ''
+    }
+  },
+  production: {
+    database: {
+      username: process.env.NODE_ORACLE_EAM_PRODUCTION_DATABASE_USERNAME || '',
+      password: process.env.NODE_ORACLE_EAM_PRODUCTION_DATABASE_PASSWORD || '',
+      hostname: process.env.NODE_ORACLE_EAM_PRODUCTION_DATABASE_HOSTNAME || '',
+      port: process.env.NODE_ORACLE_EAM_PRODUCTION_DATABASE_PORT || '',
+      serviceName: process.env.NODE_ORACLE_EAM_PRODUCTION_DATABASE_SERVICE_NAME || ''
     }
   }
 }
