deef5e035b81916add87402196778cd3802503dc
Grant Fortington <gwfortington@hotmail.com>
2018-03-04 17:02:43 +1030

    Added some new routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 
+router.use('/activity_causes', require('./activity_causes'));
+router.use('/activity_sources', require('./activity_sources'));
+router.use('/activity_types', require('./activity_types'));
 router.use('/asset_areas', require('./asset_areas'));
 router.use('/asset_categories', require('./asset_categories'));
 router.use('/asset_criticalities', require('./asset_criticalities'));
@@ -11,5 +14,6 @@ router.use('/item_types', require('./item_types'));
 router.use('/organizations', require('./organizations'));
 router.use('/query', require('./query'));
 router.use('/resources', require('./resources'));
+router.use('/shutdown_types', require('./shutdown_types'));
 
 module.exports = router;
